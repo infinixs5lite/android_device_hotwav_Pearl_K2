a7cc7586535ca34ff622b4a409d7a5c98734e2e3
infinixs5lite <kelvinchinedu97@gmail.com>
2023-05-28 20:47:16 +0100

    update

--- omni_Pearl_K2.mk
+++ omni_Pearl_K2.mk
@@ -7,14 +7,24 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from Pearl_K2 device
-$(call inherit-product, device/hotwav/Pearl_K2/device.mk)
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+        libpuresoftkeymasterdevice
 
+        TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
+            $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+                $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+                # Fastbootd
+                PRODUCT_PACKAGES += \
+                    android.hardware.fastboot@1.0-impl-mock \
+                        android.hardware.boot@1.1-impl-recovery \
+                            android.hardware.boot@1.1-impl
 PRODUCT_DEVICE := Pearl_K2
 PRODUCT_NAME := omni_Pearl_K2
 PRODUCT_BRAND := HOTWAV
@@ -26,4 +36,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-uct
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_k39tv1_64_bsp-user 9 PPR1.180610.011 1561808070 release-keys"
 
-BUILD_FINGERPRINT := HOTWAV/Pearl_K2/Pearl_K2:9/PPR1.180610.011/1561808070:user/release-keys
+BUILD_FINGERPRINT := HOTWAV/Pe
